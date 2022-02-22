c3a73a46ad2631adcebf735bc0a21ac06ab087b8
VladSL77 <vladsl@bk.ru>
2022-02-22 10:50:51 +0400

    Add Picasso

--- app/src/main/java/com/example/andersenhometaskview3/MainActivity.kt
+++ app/src/main/java/com/example/andersenhometaskview3/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -13,12 +14,14 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editTextUrlImage: EditText
+    private lateinit var editTextUrlImageStandard: EditText
+    private lateinit var editTextUrlImagePicasso: EditText
     private lateinit var imageView: ImageView
     private var urlImage = ""
     private var bitmap: Bitmap? = null
@@ -27,26 +30,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editTextUrlImage = findViewById(R.id.editTextImage)
+        editTextUrlImageStandard = findViewById(R.id.editTextImageStandard)
+        editTextUrlImagePicasso = findViewById(R.id.editTextImagePicasso)
         imageView = findViewById(R.id.imageView)
-        task = DownloadImageTask()
-        editTextUrlImage.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+        editTextUrlImageStandard.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                urlImage = editTextUrlImage.text.toString()
+                task = DownloadImageTask()
+                urlImage = editTextUrlImageStandard.text.toString()
                 try {
+                    Log.i("imageBug", urlImage)
                     bitmap = task.execute(urlImage).get()
+                    imageView.setImageBitmap(bitmap!!)
+                    editTextUrlImageStandard.text.clear()
                 } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-                if (bitmap != null) {
-                    imageView.setImageBitmap(bitmap)
-                } else {
                     Toast.makeText(
                         this,
                         getString(R.string.error_load),
                         Toast.LENGTH_LONG
                     ).show()
+                    e.printStackTrace()
                 }
+                hideKeyboard()
+                return@OnKeyListener true
+            }
+            false
+        })
+        editTextUrlImagePicasso.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                task = DownloadImageTask()
+                urlImage = editTextUrlImagePicasso.text.toString()
+                Picasso.with(this)
+                    .load(urlImage)
+                    .into(imageView, object : com.squareup.picasso.Callback {
+                        override fun onSuccess() {}
+                        override fun onError() {
+                            Toast.makeText(
+                                baseContext,
+                                getString(R.string.error_load),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+                editTextUrlImagePicasso.text.clear()
                 hideKeyboard()
                 return@OnKeyListener true
             }
